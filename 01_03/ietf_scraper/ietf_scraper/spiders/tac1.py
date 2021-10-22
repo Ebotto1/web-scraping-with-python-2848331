f1d19e5c79a541ec82e368bbbf51c222ae0f6a48
Ebotto1 <botto@iofive.com>
2021-10-22 18:35:41 -0500

    Update tac1.py

--- 01_03/ietf_scraper/ietf_scraper/spiders/tac1.py
+++ 01_03/ietf_scraper/ietf_scraper/spiders/tac1.py
@@ -8,5 +8,5 @@ class TacSpider(scrapy.Spider):
     start_urls = ['https://www.county.org/About-Texas-Counties/Texas-County-Websites']
 
     def parse(self, response):
-        return {'county': response.css('.large-15 a::text').get()
+        return {'county': response.css('.large-15 a::text').get(),
                 'Link': response.css('.large-15 a').attrib['href']}
