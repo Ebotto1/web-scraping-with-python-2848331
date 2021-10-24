af6c686983798b562f389026b9838284a38e36f7
Ebotto1 <botto@iofive.com>
2021-10-23 22:10:34 -0500

    Update and rename ctaSpider.py to ctaSpider0.py

---
+++ 01_03/ietf_scraper/ietf_scraper/spiders/ctaSpider0.py
@@ -0,0 +1,12 @@
+class CtaspiderSpider(scrapy.Spider):
+    name = 'ctaSpider'
+    allowed_domains = ['county.org']
+    start_urls = ['https://www.county.org/About-Texas-Counties/Texas-County-Websites/']
+
+    def parse(self, response):
+        return {'county': response.css('.large-15 a::text').get(),
+                'Link': response.css('.large-15 a').attrib['href']}
+
+#scrapy startproject ctaspider <website>
+#scrapy genspider
+#scrapy runspider ctaSpider.py -o cta_out.json -t json
